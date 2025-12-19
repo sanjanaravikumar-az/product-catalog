ea9910c8b27c2bcd94c241dec10aa8a653f15247
sanjanaravikumar-az <sanjrkmr@amazon.com>
2025-12-19 11:11:09 -0500

    chore: update

--- amplify/function/lowstockproductcatalog/resource.ts
+++ amplify/function/lowstockproductcatalog/resource.ts
@@ -4,6 +4,6 @@ export const lowstockproductcatalog = defineFunction({
     entry: "./handler.ts",
     timeoutSeconds: 25,
     memoryMB: 128,
-    environment: { API_PRODUCTCATALOG_GRAPHQLAPIKEYOUTPUT: "da2-luiopcy65nde3cf5eqflqwlhvy", API_PRODUCTCATALOG_GRAPHQLAPIENDPOINTOUTPUT: "https://xdlo4oa7kjbufgtnczmdhzmdma.appsync-api.us-east-1.amazonaws.com/graphql", API_PRODUCTCATALOG_GRAPHQLAPIIDOUTPUT: "q3bnmmb6p5fbhlqbljf4yexsdq", AUTH_PRODUCTCATALOG6E145452_USERPOOLID: "us-east-1_85iAHQaeM", ENV: `${branchName}`, REGION: "us-east-1" },
+    environment: { API_PRODUCTCATALOG_GRAPHQLAPIKEYOUTPUT: "da2-luiopcy65nde3cf5eqflqwlhvy", API_PRODUCTCATALOG_GRAPHQLAPIENDPOINTOUTPUT: "https://xdlo4oa7kjbufgtnczmdhzmdma.appsync-api.us-east-1.amazonaws.com/graphql", API_PRODUCTCATALOG_GRAPHQLAPIIDOUTPUT: "q3bnmmb6p5fbhlqbljf4yexsdq", AUTH_PRODUCTCATALOG6E145452_USERPOOLID: "us-east-1_85iAHQaeM", ENV: "main", REGION: "us-east-1" },
     runtime: 22
 });
